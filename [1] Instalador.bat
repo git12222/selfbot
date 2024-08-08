764694bc04286ae0c0cc634b3797bd838a4a4c50
git12222 <zxbloxofc09@gmail.com>
2024-08-08 18:02:27 -0300

    sel.

---
+++ [1] Instalador.bat
@@ -0,0 +1,40 @@
+@echo off
+color A
+title Pdrx Div
+echo.
+echo.
+echo                   ----------------------------------------------------
+echo                     Bem vindo ao instalador do Mimo Div         
+echo                     Voce esta prestes a instalar todos os requisitos  
+echo                     Em caso de duvida entre no grupo do discord 
+echo                   ----------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause nul
+cls
+color B
+cls
+call npm i chalk@4 colors discord.js@12 glob@7 node-fetch@2 fs child_process@1.0.2 sync-fetch@0.4.1
+cls
+color C
+echo.
+echo.
+echo.
+echo.
+echo                   ----------------------------------------------------
+echo                       Prontinho agora voce pode fechar o arquivo.
+echo                   ----------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause nul
+exit
